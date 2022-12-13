2f8222f5ebf3d5b5eea9e534139f3730194cc7d8
Alexandre <leitetrevisani@gmail.com>
2022-12-13 07:44:40 -0300

    criando classe abstrata CONTAS

--- banco_projeto/main.py
+++ banco_projeto/main.py
@@ -6,9 +6,9 @@ possa sacar/depositar nessa conta. Contas corrente tem um limite extra.
 Conta (ABC)
     ContaCorrente
     ContaPoupanca
-Pessoa (ABC)
+Pessoa
     Cliente
-        Clente -> Conta
+        Clente -> Conta (um para um ou um para muitos)
 Banco
     Banco -> Cliente
     Banco -> Conta
@@ -24,29 +24,29 @@ Criar classes ContaPoupanca e ContaCorrente que herdam de Conta
     polimorfismo - as subclasses que implementam o método sacar)
 Criar classe Banco para AGREGAR classes de clientes e de contas (Agregação)
 Banco será responsável autenticar o cliente e as contas da seguinte maneira:
-    Banco tem contas e clentes (Agregação)
+    Banco tem contas e clientes (Agregação)
     * Checar se a agência é daquele banco
     * Checar se o cliente é daquele banco
     * Checar se a conta é daquele banco
 Só será possível sacar se passar na autenticação do banco (descrita acima)
-Banco autentica por um método.
+Banco autentica por um método (autenticar).
 """
+import contas
+import pessoas
+from banco import Banco
 
-import abc
+c1 = pessoas.Cliente('Luiz', 30)
+cc1 = contas.ContaCorrente(111, 222, 0, 0)
+c1.conta = cc1
+c2 = pessoas.Cliente('Maria', 18)
+cp1 = contas.ContaPoupanca(112, 223, 100)
+c2.conta = cp1
+banco = Banco()
+banco.clientes.extend([c1, c2])
+banco.contas.extend([cc1, cp1])
+banco.agencias.extend([111, 222])
 
-
-class Conta(abc.ABC):
-    def __init__(self, agencia, conta, saldo):
-        self.agencia = agencia
-        self.conta = conta
-        self.saldo = saldo
-
-    @abc.abstractmethod
-    def sacar(self, valor): ...
-
-    def depositar(self, valor):
-        self.saldo += valor
-        self.detalhes(f'(DEPÓSITO {valor})')
-
-    def detalhes(self, msg=''):
-        print(f'O seu saldo é {self.saldo:.2f} {msg}')
+if banco.autenticar(c1, cc1):
+    cc1.depositar(10)
+    c1.conta.depositar(100)
+    print(c1.conta)
